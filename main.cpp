fb8d0166caa34196ae793061cd55111de2810058
dewe biag <dewebiag@gmail.com>
2024-10-17 11:21:19 -0700

    Optimized code.

--- main.cpp
+++ main.cpp
@@ -19,20 +19,14 @@ size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* buf
 }
 
 std::vector<std::string> getTotalLogs() {
-    // playerID is NOT playerUID
     std::vector<std::string> logIDs; 
-    std::string playerID = "76561198045822284"; 
+    const std::string playerID = "76561198108292940"; 
+    const std::string apiUrl = "https://logs.tf/api/v1/log?player=" + playerID;
 
-    // http://logs.tf/api/v1/log?title=X&uploader=Y&player=Z&limit=N&offset=N 
-    // should be limited if not using all of it! else it will be slow.  
-    std::string apiUrl = "https://logs.tf/api/v1/log?player=" + playerID;
-
-    CURL* curl;
-    CURLcode res;
+    CURL* curl = curl_easy_init();
     std::string responseString;
  
     curl_global_init(CURL_GLOBAL_DEFAULT);
-    curl = curl_easy_init();
 
     int totalLogs = 0;
     if(curl) {
@@ -40,19 +34,24 @@ std::vector<std::string> getTotalLogs() {
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseString);
  
-        res = curl_easy_perform(curl);
+        CURLcode res = curl_easy_perform(curl);
  
         if(res != CURLE_OK) {
             std::cerr << "Failed." << curl_easy_strerror(res) << std::endl;
         } else {
-            auto jsonResponse = nlohmann::json::parse(responseString);
-            totalLogs = jsonResponse["total"]; 
 
-            for(int i = 0; i < totalLogs - 1; i++) {
-                if(jsonResponse["logs"][i]["players"] == 12 && jsonResponse["logs"][i]["id"] > 435070) {
-                    int logID = jsonResponse["logs"][i]["id"]; 
-                    logIDs.push_back(std::to_string(logID));
+            try {
+                auto jsonResponse = nlohmann::json::parse(responseString);
+                totalLogs = jsonResponse["total"]; 
+
+                for(int i = 0; i < totalLogs - 1; i++) {
+                    const auto& log = jsonResponse["logs"][i];
+                    if (log["players"] == 12 && log["id"] > 2672567) {
+                        logIDs.push_back(std::to_string(log["id"].get<int>()));
+                    }
                 }
+            } catch(const nlohmann::json::exception& e) {
+                std::cerr << "JSON Error: " << e.what() << std::endl; 
             }
          }
  
@@ -63,10 +62,21 @@ std::vector<std::string> getTotalLogs() {
     return logIDs; 
 }
 
-std::vector<std::pair<std::string, float>> getDPM() {
-    std::string playerUID = "[U:1:85556556]"; 
-    std::vector<std::string> logIDs = getTotalLogs();
+std::vector<std::pair<std::string, float>> getTopFiveWorstLogs(std::vector<std::pair<std::string, float>> ratioVectorDPM) {
+    std::sort(ratioVectorDPM.begin(), ratioVectorDPM.end(), [](const std::pair<std::string, float>& a, const std::pair<std::string, float>& b) {
+            return a.second < b.second; 
+    });
 
+    if (ratioVectorDPM.size() > 5) {
+        ratioVectorDPM.resize(5);
+    }
+
+    return ratioVectorDPM; 
+}
+
+std::vector<std::pair<std::string, float>> getDPM() {
+    const std::string playerUID = "[U:1:148027212]"; 
+    const std::vector<std::string> logIDs = getTotalLogs();
     const size_t totalLogs = logIDs.size(); 
     const size_t batchSize = 8; 
 
@@ -87,44 +97,49 @@ std::vector<std::pair<std::string, float>> getDPM() {
         std::vector<std::string> responseStrings(curBatchSize);  
 
         for (int i = 0; i < curBatchSize; i++) {
-            std::string logID = logIDs[start + i];
-            std::string apiUrl = "https://logs.tf/api/v1/log/" + logID;
+            std::string apiUrl = "https://logs.tf/api/v1/log/" + logIDs[start + i];;
 
             curls[i] = curl_easy_init();
             curl_easy_setopt(curls[i], CURLOPT_URL, apiUrl.c_str());
             curl_easy_setopt(curls[i], CURLOPT_WRITEFUNCTION, WriteCallback);
             curl_easy_setopt(curls[i], CURLOPT_WRITEDATA, &responseStrings[i]);
-
             curl_multi_add_handle(multi_handle, curls[i]);
         }
 
         int still_running = 0;
-        do {
+        curl_multi_perform(multi_handle, &still_running);
+
+        while (still_running) {
+            curl_multi_wait(multi_handle, nullptr, 0, 1000, nullptr);
             curl_multi_perform(multi_handle, &still_running);
-        } while (still_running);
+        }
 
         for(int i = 0; i < curBatchSize; i++) {        
             curl_multi_remove_handle(multi_handle, curls[i]);
             curl_easy_cleanup(curls[i]);
 
-            auto jsonResponse = nlohmann::json::parse(responseStrings[i]);
+            try {
+                auto jsonResponse = nlohmann::json::parse(responseStrings[i]);
 
-            auto allPlayers = jsonResponse["players"];
-            int userDPM = jsonResponse["players"][playerUID]["dapm"];
- 
-            std::vector<int> allPlayerDPM; 
-            for (nlohmann::json::iterator it = allPlayers.begin(); it != allPlayers.end(); ++it) {
-                auto player = it.value();
-                if(player["class_stats"][0]["type"] != "medic" && player["class_stats"][0]["total_time"] > 1200) {
-                    allPlayerDPM.push_back(player["dapm"]); 
+                auto allPlayers = jsonResponse["players"];
+                int userDPM = jsonResponse["players"][playerUID]["dapm"];
+    
+                std::vector<int> allPlayerDPM; 
+                for (nlohmann::json::iterator it = allPlayers.begin(); it != allPlayers.end(); ++it) {
+                    auto player = it.value();
+                    if(player["class_stats"][0]["type"] != "medic" && player["class_stats"][0]["total_time"] > 1200) {
+                        allPlayerDPM.push_back(player["dapm"]); 
+                    }
                 }
-            }
 
-            if (!allPlayerDPM.empty() && jsonResponse["players"][playerUID]["class_stats"][0]["type"] != "medic") { 
-                float avgDPM = std::accumulate(allPlayerDPM.begin(), allPlayerDPM.end(), 0.0f) / allPlayerDPM.size();
-                float ratioDPM = userDPM / avgDPM; 
-                std::cout << "Log ID: " << logIDs[start + i] << ", DPM: " <<  ratioDPM << std::endl;
-                ratioVectorDPM.push_back(std::make_pair(logIDs[start + i], ratioDPM));  
+                if (!allPlayerDPM.empty() && jsonResponse["players"][playerUID]["class_stats"][0]["type"] != "medic") { 
+                    float avgDPM = std::accumulate(allPlayerDPM.begin(), allPlayerDPM.end(), 0.0f) / allPlayerDPM.size();
+                    float ratioDPM = userDPM / avgDPM; 
+                    // std::cout << "Log ID: " << logIDs[start + i] << ", DPM: " <<  ratioDPM << std::endl;
+                    ratioVectorDPM.push_back(std::make_pair(logIDs[start + i], ratioDPM));  
+                }
+            } catch(const nlohmann::json::exception& e) {
+                std::cerr << "JSON Error: " << e.what() << std::endl; 
             }
         }
 
@@ -133,15 +148,11 @@ std::vector<std::pair<std::string, float>> getDPM() {
 
     curl_multi_cleanup(multi_handle); 
 
-    return ratioVectorDPM; 
+    return getTopFiveWorstLogs(ratioVectorDPM); 
 }
 
 int main() {
     std::vector<std::pair<std::string, float>> ratioVectorDPM = getDPM(); 
-
-    std::sort(ratioVectorDPM.begin(), ratioVectorDPM.end(), [](const std::pair<std::string, float>& a, const std::pair<std::string, float>& b) {
-            return a.second > b.second; 
-    });
 
     for(const std::pair<std::string, float>& ratioDPM : ratioVectorDPM) {
         std::cout << "https://logs.tf/" << ratioDPM.first << ", DPM: " <<  ratioDPM.second << std::endl; 
